8cc72d1eb9e076bb5943a1c1279e0b1f46c1a787
mmaahmedkhannn <mahmedkhan.university@gmail.com>
2026-03-06 19:41:41 +0500

    Auto-update from local IDE

--- test-parse.js
+++ test-parse.js
@@ -1,18 +1,23 @@
-const fs = require('fs');
-const str = fs.readFileSync('test.html', 'utf8');
+async function test() {
+    const formattedUrl = "https://www.pinterest.com/pin/922401498611/";
+    const response = await fetch(formattedUrl, {
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml",
+            "Accept-Language": "en-US,en;q=0.5"
+        }
+    });
 
-const jpgs = str.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\/_%-]+\.jpg/g) || [];
-const escapedJpgs = str.match(/https:\\\/\\\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\\\/_%-]+\.jpg/g) || [];
+    console.log("Status:", response.status);
+    const html = await response.text();
+    console.log("HTML length:", html.length);
+    const origUrlsMatch = html.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com\/originals\/[A-Za-z0-9.\/_%-]+\.jpg/g);
+    console.log("Matches:", origUrlsMatch ? origUrlsMatch.length : 0);
 
-console.log("Normal JPGs:", Array.from(new Set(jpgs)).slice(0, 10));
-
-const unescaped = escapedJpgs.map(s => s.replace(/\\\//g, '/'));
-console.log("Escaped JPGs:", Array.from(new Set(unescaped)).slice(0, 5));
-
-const mp4s = str.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\/_%-]+\.mp4/g) || [];
-const escapedMp4s = str.match(/https:\\\/\\\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\\\/_%-]+\.mp4/g) || [];
-
-console.log("Normal MP4s:", Array.from(new Set(mp4s)).slice(0, 5));
-
-const unescapedMp4 = escapedMp4s.map(s => s.replace(/\\\//g, '/'));
-console.log("Escaped MP4s:", Array.from(new Set(unescapedMp4)).slice(0, 5));
+    if (origUrlsMatch) {
+        console.log(Array.from(new Set(origUrlsMatch)));
+    } else {
+        require('fs').writeFileSync('fail.html', html);
+    }
+}
+test();
