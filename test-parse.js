5da2e93ccc242efd2e2837663c50e0fd6a29029a
mmaahmedkhannn <mahmedkhan.university@gmail.com>
2026-03-06 19:40:13 +0500

    Auto-update from local IDE

--- test-parse.js
+++ test-parse.js
@@ -1,7 +1,18 @@
 const fs = require('fs');
+const str = fs.readFileSync('test.html', 'utf8');
 
-const str = fs.readFileSync('formatted.json', 'utf8');
+const jpgs = str.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\/_%-]+\.jpg/g) || [];
+const escapedJpgs = str.match(/https:\\\/\\\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\\\/_%-]+\.jpg/g) || [];
 
-console.log("Length:", str.length);
-console.log("Preview:");
-console.log(str.substring(0, 1000));
+console.log("Normal JPGs:", Array.from(new Set(jpgs)).slice(0, 10));
+
+const unescaped = escapedJpgs.map(s => s.replace(/\\\//g, '/'));
+console.log("Escaped JPGs:", Array.from(new Set(unescaped)).slice(0, 5));
+
+const mp4s = str.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\/_%-]+\.mp4/g) || [];
+const escapedMp4s = str.match(/https:\\\/\\\/[A-Za-z0-9.-]+\.pinimg\.com[A-Za-z0-9.\\\/_%-]+\.mp4/g) || [];
+
+console.log("Normal MP4s:", Array.from(new Set(mp4s)).slice(0, 5));
+
+const unescapedMp4 = escapedMp4s.map(s => s.replace(/\\\//g, '/'));
+console.log("Escaped MP4s:", Array.from(new Set(unescapedMp4)).slice(0, 5));
