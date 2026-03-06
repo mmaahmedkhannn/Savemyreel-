9bfb4256a1e8b0b3ddcf8463bdc68eca4b0b21dd
mmaahmedkhannn <mahmedkhan.university@gmail.com>
2026-03-07 02:04:55 +0500

    Auto-update from local IDE

--- lib/services/pinterest.ts
+++ lib/services/pinterest.ts
@@ -40,10 +40,12 @@ export const pinterestService: DownloaderService = {
                     return new Promise((resolve, reject) => {
                         const req = https.get(targetUrl, {
                             headers: {
-                                "User-Agent": "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)",
-                                "Accept": "text/html",
+                                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0",
+                                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                                "Accept-Language": "en-US,en;q=0.5",
                                 "Accept-Encoding": "gzip, deflate, br",
-                                "Connection": "close"
+                                "Connection": "Keep-Alive",
+                                "Upgrade-Insecure-Requests": "1"
                             }
                         }, (res: any) => {
                             if (res.statusCode >= 300 && res.statusCode <= 308 && res.headers.location) {
