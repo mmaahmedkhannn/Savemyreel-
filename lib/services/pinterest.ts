13a44193fbbcd4d0e25066f82dd733900a5173ae
mmaahmedkhannn <mahmedkhan.university@gmail.com>
2026-03-07 00:47:08 +0500

    Auto-update from local IDE

--- lib/services/pinterest.ts
+++ lib/services/pinterest.ts
@@ -45,6 +45,11 @@ export const pinterestService: DownloaderService = {
                         "Accept-Language": "en-US,en;q=0.5",
                         "Cache-Control": "no-cache",
                         "Pragma": "no-cache",
+                        "Sec-Fetch-Dest": "document",
+                        "Sec-Fetch-Mode": "navigate",
+                        "Sec-Fetch-Site": "none",
+                        "Sec-Fetch-User": "?1",
+                        "Upgrade-Insecure-Requests": "1"
                     },
                     redirect: 'follow'
                 });
