7cb2275297d3d000f0252869572e51fad5f5d793
mmaahmedkhannn <mahmedkhan.university@gmail.com>
2026-03-06 19:43:27 +0500

    Auto-update from local IDE

--- test-parse.js
+++ test-parse.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const html = fs.readFileSync('fail.html', 'utf8');
-const urls = html.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com\/[A-Za-z0-9.\/_%-]+/g) || [];
+const urls = html.match(/https:\/\/[A-Za-z0-9.-]+\.pinimg\.com\/[A-Za-z0-9.\/_%-]+\.jpg/g) || [];
 const uniqueUrls = Array.from(new Set(urls));
+console.log("JPGs found:", uniqueUrls.length);
 console.log(uniqueUrls.slice(0, 20));
