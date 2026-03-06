5903a3d79ce6b4576da2ded585001676c8a60746
mmaahmedkhannn <mahmedkhan.university@gmail.com>
2026-03-06 17:57:19 +0500

    Auto-update from local IDE

--- test-pinterest.js
+++ test-pinterest.js
@@ -1,17 +1,19 @@
 const { execFile } = require('child_process');
 const path = require('path');
 
-const url = "https://www.pinterest.com/ideas/franz-kafka-funny/922268612745/";
+const url1 = "https://www.pinterest.com/ideas/franz-kafka-funny/922268612745/";
+const url2 = "https://www.pinterest.com/pin/922268612745/";
 
 const ytdlpPath = path.join(__dirname, "bin", "yt-dlp.exe");
 
-execFile(ytdlpPath, ["--dump-json", url, "--no-warnings"], { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
-    if (err) {
-        console.error("ERROR:");
-        console.error(stderr);
-    } else {
-        const data = JSON.parse(stdout);
-        console.log("SUCCESS:");
-        console.log(data.title, data.url || data.thumbnail);
-    }
+execFile(ytdlpPath, ["--dump-json", url1, "--no-warnings"], { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
+    console.log("=== URL 1 (/ideas/) ===");
+    if (err) console.log("ERROR:", stderr || err.message);
+    else console.log("SUCCESS");
+});
+
+execFile(ytdlpPath, ["--dump-json", url2, "--no-warnings"], { maxBuffer: 10 * 1024 * 1024 }, (err, stdout, stderr) => {
+    console.log("=== URL 2 (/pin/) ===");
+    if (err) console.log("ERROR:", stderr || err.message);
+    else console.log("SUCCESS");
 });
